bba8553f19f5d6bdb550f43fa959f785561768fc
justSmK <semko431@gmail.com>
2022-07-27 08:22:00 +0300

    Added description in English

--- Yandex Algorithms.playground/Pages/Lection 1.xcplaygroundpage/Contents.swift
+++ Yandex Algorithms.playground/Pages/Lection 1.xcplaygroundpage/Contents.swift
@@ -1,13 +1,18 @@
 /*:
  ### Дана строка в кодировке UTF-8
+ ### A string in UTF-8 encoding is given
  
  ### Найти самый часто встречающийся в ней символ. Если несколько символов встречаются одинаково часто, то можно вывести любой
+ ### Find the most frequent character in it. If several characters are equally frequent, any character can be printed
  */
 
 /*:
  ### Решение 1
+ ### Solution 1
  
  Переберём все позиции и для каждой позиции в строке ещё раз переберём все позиции и в случае совпадения прибавим к счётчику единицу. Найдём максимальное значение счётчика
+ 
+ Go through all the positions and for each position in the string go through all the positions again and if they match, add one to the counter. Find the maximal value of the counter
  
  O(N^2)
  */
@@ -39,8 +44,9 @@ mostCommonSymbol1(string: stringTask1)
 
 /*:
  ### Решение 2
- 
+ ### Solution 2
 
  
  O(N)
  */
+
